e2c0bbc74ebf3e5c391e64e6078e54ff9daff057
Pavel <toruchan174@gmail.com>
2024-07-16 00:09:13 +0300

    Add Quick layout
    This is not working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include <QApplication>
-#include <QMessageBox>
 
-#include "inteface/src/nodes/node.hpp"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main(int argc, char** argv) {
-    QApplication app {argc, argv};
-    QMessageBox::about(nullptr, "Hello, World!", "Hello, World!");
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"interface/ui/main.qml"_qs);
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection
+    );
+    engine.load(url);
+
     return app.exec();
 }
